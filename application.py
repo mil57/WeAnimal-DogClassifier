0fafd504c9e41c22eb4ce716d24c7e4cc25a40bf
mil57 <32526142+mil57@users.noreply.github.com>
2019-05-20 11:07:15 -0700

    Add stuff

--- application.py
+++ application.py
@@ -1,3 +1,16 @@
+# Machine Learning Stuff
+'''
+import keras
+import tensorflow as tf
+from keras.models import model_from_json
+import json
+import os
+from PIL import Image
+from skimage.io import imread
+from keras.applications.densenet import DenseNet121, preprocess_input
+import numpy as np
+'''
+
 from flask import Flask
 app = Flask(__name__)
 
